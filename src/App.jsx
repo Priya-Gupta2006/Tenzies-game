cfbb647b2b99880f3751cf5b2de4354bd63b1bc8
Priya Gupta <6guptapriya@gmail.com>
2026-04-29 00:26:35 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Die from "./Die.jsx"
+
+
+export default function App(){
+
+  function generateAllNewDice(){
+    return new Array(10)
+          .fill(0)
+          .map(()=> Math.ceil(Math.random()*6))
+
+  }
+
+  console.log(generateAllNewDice());
+
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+      </div>
+    </main>
+  )
+}
