6437335f05df3fa786b9c431e9ca5765bd9cc82f
Priya Gupta <6guptapriya@gmail.com>
2026-05-01 13:06:55 +0530

    Tenzies Acessibility improvements

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,9 @@ export default function App() {
     return (
         <main>
             {gameWon && <Confetti />}
+            <div className="sr-only">
+                {gameWon && <p>Congratulations! You Won. Press New Game to start again</p>}
+            </div>
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
--- src/Die.jsx
+++ src/Die.jsx
@@ -4,6 +4,14 @@ export default function Die(props) {
     }
     
     return (
-        <button onClick={props.hold} style={styles}>{props.value}</button>
+        <button 
+            onClick={props.hold} 
+            style={styles}
+            aria-pressed={props.isHeld}
+            aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+            >
+        {props.value}
+        </button>
     )
 }
