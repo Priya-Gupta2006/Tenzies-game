1753a3cbdb603f4e756aa293f3852d259e753001
Priya Gupta <6guptapriya@gmail.com>
2026-05-01 12:25:57 +0530

    installed confetti and used the confetti component once the user wins the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import confetti from "react-confetti"
 
 export default function App() {
     const [dice, setDice] = useState(generateAllNewDice())
@@ -48,6 +49,7 @@ export default function App() {
 
     return (
         <main>
+            {gameWon && <confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
